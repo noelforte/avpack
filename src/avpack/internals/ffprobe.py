b9d6503685b38b2fe8adfb19bb2c573047927cef
Noel Forte <noel@forte.is>
2025-05-12 00:55:09 -0400

    refactor types and required/optional properties on ffprobe data

--- src/avpack/internals/ffprobe.py
+++ src/avpack/internals/ffprobe.py
@@ -1,7 +1,8 @@
 import shutil
 from subprocess import SubprocessError, run
+from typing import Annotated
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Discriminator, Tag
 
 FFPROBE = shutil.which("ffprobe")
 FFPROBE_ARGS = [
@@ -19,15 +20,14 @@ class Stream(BaseModel):
     index: int
     codec_type: str
     time_base: str
-    tags: dict[str, str]
-    disposition: dict[str, bool]
-    start_time: float
+    tags: dict[str, str] | None = None
+    disposition: dict[str, bool] | None = None
+    start_time: float | None = None
 
 
 class VideoStream(Stream):
     """A video stream"""
 
-    codec_type: str = "video"
     width: int
     height: int
     r_frame_rate: str
@@ -36,16 +36,13 @@ class VideoStream(Stream):
 class AudioStream(Stream):
     """An audio stream"""
 
-    codec_type: str = "audio"
     sample_rate: int
     channels: int
-    channel_layout: str
+    channel_layout: str | None = None
 
 
 class SubtitleStream(Stream):
     """A subtitle stream"""
-
-    codec_type: str = "subtitle"
 
 
 class Chapter(BaseModel):
@@ -57,22 +54,36 @@ class Chapter(BaseModel):
     end: int
     start_time: float
     end_time: float
-    tags: dict[str, str]
+    tags: dict[str, str] | None = None
 
 
 class Format(BaseModel):
     """A media container format"""
 
     nb_streams: int
-    start_time: float
     duration: float
-    tags: dict[str, str]
+    start_time: float | None = None
+    tags: dict[str, str] | None = None
+
+
+def disambiguate_stream_types(v: dict[str, str] | Stream):
+    codec = v.get("codec_type") if isinstance(v, dict) else v.codec_type
+    return codec if codec in ("video", "audio", "subtitle") else "..."
 
 
 class ProbeData(BaseModel):
     """An object containing ffprobe data"""
 
-    streams: tuple[VideoStream | AudioStream | SubtitleStream | Stream, ...]
+    streams: tuple[
+        Annotated[
+            Annotated[VideoStream, Tag("video")]
+            | Annotated[AudioStream, Tag("audio")]
+            | Annotated[SubtitleStream, Tag("subtitle")]
+            | Annotated[Stream, Tag("...")],
+            Discriminator(disambiguate_stream_types),
+        ],
+        ...,
+    ]
     chapters: tuple[Chapter, ...]
     format: Format
 
@@ -86,3 +97,23 @@ class ProbeData(BaseModel):
         )
 
         return cls.model_validate_json(probe.stdout)
+
+    @property
+    def video_streams(self) -> tuple[VideoStream, ...]:
+        return tuple(
+            stream for stream in self.streams if isinstance(stream, VideoStream)
+        )
+
+    @property
+    def audio_streams(self) -> tuple[AudioStream, ...]:
+        return tuple(
+            stream for stream in self.streams if isinstance(stream, AudioStream)
+        )
+
+    @property
+    def subtitle_streams(self) -> tuple[SubtitleStream, ...]:
+        return tuple(
+            stream
+            for stream in self.streams
+            if isinstance(stream, SubtitleStream)
+        )
